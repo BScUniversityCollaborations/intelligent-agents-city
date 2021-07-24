3cbe587b5aa67f4485dd699d9a9c425ece1e6aec
Νικόλας Π <nikolas.98@live.com>
2021-07-24 14:07:12 +0300

    Code

---
+++ Intelligent Agents City/Assets/Scripts/Global.cs
@@ -0,0 +1,17 @@
+public static class Global
+{
+    public static int npcEnergy1 = 100;
+    public static int npcEnergy2 = 100;
+    public static int npcEnergy3 = 100;
+    public static int npcEnergy4 = 100;
+
+    public static double npcGold1 = 0.0;
+    public static double npcGold2 = 0.0;
+    public static double npcGold3 = 0.0;
+    public static double npcGold4 = 0.0;
+
+    public static double npcExploration1 = 0.0;
+    public static double npcExploration2 = 0.0;
+    public static double npcExploration3 = 0.0;
+    public static double npcExploration4 = 0.0;
+}
