e9b8051b05c4939bb16ad3fb8c2b9d52b82b3987
zartilas <36131492+zartilas@users.noreply.github.com>
2021-07-24 17:23:16 +0300

    No bug Fixes

--- Intelligent Agents City/Assets/Scripts/move.cs
+++ Intelligent Agents City/Assets/Scripts/move.cs
@@ -25,12 +25,12 @@ public class move : MonoBehaviour
     void Update()
     {
        transform.position = Vector2.Lerp(transform.position, end.position, Time.deltaTime);
-        rectangle = GetComponent<RectTransform>();
+    /*    rectangle = GetComponent<RectTransform>();
         if (rectangle.rect.Contains(Input.GetTouch(0).deltaPosition))
         {
 
             transform.position = Vector2.Lerp(rectangle.position, start.position, Time.deltaTime);
-        }
+        }*/
     }
 
     private void OnDrawGizmos()
