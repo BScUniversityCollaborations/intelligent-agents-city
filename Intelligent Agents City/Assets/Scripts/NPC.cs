baf0ba6af523221a16e6f31277114e30a7e1d168
Νικόλας Π <nikolas.98@live.com>
2021-07-25 20:38:59 +0300

    changes

--- Intelligent Agents City/Assets/Scripts/NPC.cs
+++ Intelligent Agents City/Assets/Scripts/NPC.cs
@@ -2,8 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RandomItemGenerator : MonoBehaviour
+public class NPC : MonoBehaviour
 {
+    // Declaring variables for default values
+    public int defaultGold = 0;
+    public float defaultEnergy = 100.0f;
+    public float defaultExploration = 0.0f;
+    public int defaultEnergyPots = 0;
+
     // Start is called before the first frame update
     void Start()
     {
