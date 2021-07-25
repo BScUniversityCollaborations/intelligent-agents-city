519a21e8b9c45777990a8bb375718eed5df96f9e
Νικόλας Π <nikolas.98@live.com>
2021-07-25 21:26:51 +0300

    File organization

--- Intelligent Agents City/Assets/Scripts/NPC.cs
+++ Intelligent Agents City/Assets/Scripts/NPC.cs
@@ -10,15 +10,49 @@ public class NPC : MonoBehaviour
     public float defaultExploration = 0.0f;
     public int defaultEnergyPots = 0;
 
+    //variables that will be used during combat
+    public int npcGold;
+    public int NpcGold
+    {
+        get { return npcGold; }
+        set { npcGold = value; }
+    }
+
+    public float npcEnergy;
+    public float NpcEnergy
+    {
+        get { return npcEnergy; }
+        set { npcEnergy = value; }
+    }
+
+    public float npcExploration;
+    public float NpcExploration
+    {
+        get { return npcExploration; }
+        set { npcExploration = value; }
+    }
+
+    public int npcEnergyPots;
+    public int NpcEnergyPots
+    {
+        get { return npcEnergyPots; }
+        set { npcEnergyPots = value; }
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //initialize the variables that will be used by the monsters
+        npcGold = defaultGold;
+        npcEnergy = defaultEnergy;
+        npcExploration = defaultExploration;
+        npcEnergyPots = defaultEnergyPots;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        npcEnergy -= 1 * Time.deltaTime;
+
     }
 }
