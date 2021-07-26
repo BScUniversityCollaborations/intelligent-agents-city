85ce061505f61db7fb398ec67ceba5093bf80914
Νικόλας Π <nikolas.98@live.com>
2021-07-26 03:20:59 +0300

    Fixed UI

--- Intelligent Agents City/Assets/Scripts/NPC.cs
+++ Intelligent Agents City/Assets/Scripts/NPC.cs
@@ -1,38 +1,41 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
+using TMPro;
 using UnityEngine;
 
 public class NPC : MonoBehaviour
 {
     // Declaring variables for default values
     public int defaultGold = 0;
-    public float defaultEnergy = 100.0f;
+    public float defaultEnergy = 10.0f;
     public float defaultExploration = 0.0f;
     public int defaultEnergyPots = 0;
 
+    [SerializeField] TextMeshProUGUI textMeshProEnergy;
+    [SerializeField] TextMeshProUGUI textMeshProGold;
+
     //variables that will be used during combat
-    public int npcGold;
+    [SerializeField] public int npcGold;
     public int NpcGold
     {
         get { return npcGold; }
         set { npcGold = value; }
     }
 
-    public float npcEnergy;
+    [SerializeField] public float npcEnergy;
     public float NpcEnergy
     {
         get { return npcEnergy; }
         set { npcEnergy = value; }
     }
 
-    public float npcExploration;
+    [SerializeField] public float npcExploration;
     public float NpcExploration
     {
         get { return npcExploration; }
         set { npcExploration = value; }
     }
 
-    public int npcEnergyPots;
+    [SerializeField] public int npcEnergyPots;
     public int NpcEnergyPots
     {
         get { return npcEnergyPots; }
@@ -42,17 +45,53 @@ public class NPC : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //initialize the variables that will be used by the monsters
+        // Initialize the variables that will be used by the monsters
         npcGold = defaultGold;
         npcEnergy = defaultEnergy;
         npcExploration = defaultExploration;
         npcEnergyPots = defaultEnergyPots;
+        if (name == "NPC_1")
+        {
+            textMeshProEnergy = GameObject.Find("Txt_NPC_1_Energy_Value")
+                                          .GetComponent<TextMeshProUGUI>();
+            textMeshProGold = GameObject.Find("Txt_NPC_1_Gold_Value")
+                                          .GetComponent<TextMeshProUGUI>();
+        }
+        else if (name == "NPC_2")
+        {
+            textMeshProEnergy = GameObject.Find("Txt_NPC_2_Energy_Value")
+                                          .GetComponent<TextMeshProUGUI>();
+            textMeshProGold = GameObject.Find("Txt_NPC_2_Gold_Value")
+                                          .GetComponent<TextMeshProUGUI>();
+        }
+        else if (name == "NPC_3")
+        {
+            textMeshProEnergy = GameObject.Find("Txt_NPC_3_Energy_Value")
+                                          .GetComponent<TextMeshProUGUI>();
+            textMeshProGold = GameObject.Find("Txt_NPC_3_Gold_Value")
+                                          .GetComponent<TextMeshProUGUI>();
+        }
+        else
+        {
+            textMeshProEnergy = GameObject.Find("Txt_NPC_4_Energy_Value")
+                                          .GetComponent<TextMeshProUGUI>();
+            textMeshProGold = GameObject.Find("Txt_NPC_4_Gold_Value")
+                                          .GetComponent<TextMeshProUGUI>();
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
+
         npcEnergy -= 1 * Time.deltaTime;
+
+        if (npcEnergy >= 0.0f)
+            textMeshProEnergy.SetText("Energy: {0}%", (float)Math.Round(npcEnergy * 100f) / 100f);
+        textMeshProGold.SetText("Gold: {0}", NpcGold);
+
+        if (npcEnergy == 0.0f)
+            GameObject.Destroy(this);
 
     }
 }
