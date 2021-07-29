7249251b90551086d4334e967725675dbc38fb24
zartilas <36131492+zartilas@users.noreply.github.com>
2021-07-29 17:59:15 +0300

    Update moveRandom.cs

--- Intelligent Agents City/Assets/Scripts/moveRandom.cs
+++ Intelligent Agents City/Assets/Scripts/moveRandom.cs
@@ -16,7 +16,7 @@ public class moveRandom : MonoBehaviour
     internal float decisionTimeCount = 0;
 
     // The possible directions that the object can move int, right, left, up, down, and zero for staying in place. I added zero twice to give a bigger chance if it happening than other directions
-    internal Vector3[] moveDirections = new Vector3[] { Vector3.right, Vector3.left, Vector3.forward, Vector3.back, Vector3.zero, Vector3.zero };
+    internal Vector3[] moveDirections = new Vector3[] { Vector3.right, Vector3.left, Vector3.up, Vector3.down /*, Vector3.zero, Vector3.zero*/ };
     internal int currentMoveDirection;
 
     // Use this for initialization
@@ -39,7 +39,7 @@ public class moveRandom : MonoBehaviour
         // Move the object in the chosen direction at the set speed
         thisTransform.position += moveDirections[currentMoveDirection] * Time.deltaTime * moveSpeed;
 
-        if (decisionTimeCount > 0) decisionTimeCount -= Time.deltaTime;
+        if (decisionTimeCount > 0)  decisionTimeCount -= Time.deltaTime; 
         else
         {
             // Choose a random time delay for taking a decision ( changing direction, or standing in place for a while )
