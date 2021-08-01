283fd796593471cc7f495f7490296f1cb4d05c7a
Νικόλας Π <nikolas.98@live.com>
2021-08-01 21:00:11 +0300

    grid

--- Intelligent Agents City/Assets/Scripts/CreateGrid.cs
+++ Intelligent Agents City/Assets/Scripts/CreateGrid.cs
@@ -5,21 +5,26 @@ using UnityEngine;
 
 public class CreateGrid : MonoBehaviour
 {
-    [SerializeField] Canvas mapAreaCanvas;
+    [SerializeField] GameObject mapAreaCanvas;
     [SerializeField] float mapAreaHeight;
     [SerializeField] float mapAreaWidth;
 
     // Start is called before the first frame update
     void Start()
     {
-        mapAreaCanvas = GameObject.Find("Map_Area")
-                                  .GetComponent<Canvas>();
+        mapAreaCanvas = GameObject.Find("Map_Area");
 
         mapAreaHeight = mapAreaCanvas.GetComponent<RectTransform>().rect.height;
         mapAreaWidth = mapAreaCanvas.GetComponent<RectTransform>().rect.width;
 
         Debug.Log(mapAreaHeight + " " + mapAreaWidth);
 
-        Grid grid = new Grid((int)Math.Round(mapAreaHeight), (int)Math.Round(mapAreaWidth), 2f);
+        // Grid grid = new Grid(4, 2, 10f);
+        Grid grid = new Grid(
+            (int)Math.Round(mapAreaHeight),
+            (int)Math.Round(mapAreaWidth),
+            .9f,
+            new Vector3(-258, -192)
+        );
     }
 }
--- Intelligent Agents City/Assets/Scripts/Grid.cs
+++ Intelligent Agents City/Assets/Scripts/Grid.cs
@@ -9,12 +9,14 @@ public class Grid
     private int height;
     private float cellSize;
     private int[,] gridArray;
+    private Vector3 originPos;
 
-    public Grid(int height, int width, float cellSize)
+    public Grid(int height, int width, float cellSize, Vector3 originPos)
     {
         this.height = height;
         this.width = width;
         this.cellSize = cellSize;
+        this.originPos = originPos;
 
         gridArray = new int[width, height];
 
@@ -25,18 +27,56 @@ public class Grid
                 UtilsClass.CreateWorldText(
                     gridArray[x, y].ToString(),
                     null,
-                    getWorldPosition(x, y),
-                    20,
+                    GetWorldPosition(x, y) + new Vector3(cellSize, cellSize) * .5f,
+                    10,
                     Color.white,
                     TextAnchor.MiddleCenter
                 );
+                Debug.DrawLine(GetWorldPosition(x, y), GetWorldPosition(x, y + 1), Color.white, 100f);
+                Debug.DrawLine(GetWorldPosition(x, y), GetWorldPosition(x + 1, y), Color.white, 100f);
             }
         }
+        Debug.DrawLine(
+            GetWorldPosition(0, height),
+            GetWorldPosition(width, height),
+            Color.white,
+            100f);
+        Debug.DrawLine(
+            GetWorldPosition(width, 0),
+            GetWorldPosition(width, height),
+            Color.white,
+            100f);
     }
 
-    public Vector3 getWorldPosition(int x, int y)
+    public Vector3 GetWorldPosition(int x, int y)
     {
-        return new Vector3(x, y) * cellSize;
+        return new Vector3(x, y) * cellSize + originPos;
     }
 
+    public void SetValue(int x, int y, int value)
+    {
+        if (x >= 0 && y >= 0 && x < width && y < height)
+            gridArray[x, y] = value;
+    }
+
+    public int GetValue(int x, int y)
+    {
+        if (x >= 0 && y >= 0 && x < width && y < height)
+            return gridArray[x, y];
+        else
+            return 0;
+    }
+
+    public int GetValue(Vector3 worldPosition)
+    {
+        int x, y;
+        GetXY(worldPosition, out x, out y);
+        return GetValue(x, y);
+    }
+
+    public void GetXY(Vector3 worldPosition, out int x, out int y)
+    {
+        x = Mathf.FloorToInt((worldPosition - originPos).x / cellSize);
+        y = Mathf.FloorToInt((worldPosition - originPos).y / cellSize);
+    }
 }
