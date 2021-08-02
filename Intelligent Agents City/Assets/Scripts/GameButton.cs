8a014ff2c50d24ff2503b40d06da21c6026a791d
zartilas <36131492+zartilas@users.noreply.github.com>
2021-08-02 14:11:31 +0300

    Imported Buttons in game

---
+++ Intelligent Agents City/Assets/Scripts/GameButton.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class GameButton : MonoBehaviour
+{
+    bool isPause = false;
+    public void ReloadGame()
+    {
+      Application.LoadLevel(Application.loadedLevel);
+      Time.timeScale = 1;
+      isPause = false;
+    }
+
+    public void BackToMenu()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+
+    public void PauseGame()
+    {
+        if (isPause)
+        {
+            Time.timeScale = 1;
+            isPause = false;
+        }else
+        {
+            Time.timeScale = 0;
+            isPause = true;
+        }
+    }
+}
